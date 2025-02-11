f1cb82511f7e319e3c55f92c8f02a5ab7ddfe67e
Gustavo Sales <gus@codegus.com.br>
2025-02-11 19:24:24 -0300

    updates

--- src/app/_components/search-fields.tsx
+++ src/app/_components/search-fields.tsx
@@ -27,32 +27,36 @@ export default function SearchFields() {
   return (
     <div className="flex flex-col gap-2">
       <pre>Busca: {q}</pre>
-      <pre>User Type: {userType}</pre>
+      <pre>Tipo de usuário: {userType}</pre>
       <hr />
-      <div className="flex gap-2">
+      <div className="flex items-end gap-2">
         <div className="flex flex-col gap-1">
           <label htmlFor="q">Busca</label>
           <input
             id="q"
             type="text"
+            className="border rounded h-10 px-2"
             value={q}
             onChange={(e) => setQ(e.target.value)}
-            className="border rounded p-2"
           />
         </div>
         <div className="flex flex-col gap-1">
           <label htmlFor="userType">Tipo de usuário</label>
           <select
             id="userType"
-            value={userType ?? ""}
+            className="border rounded h-10 px-2"
+            value={userType}
             onChange={(e) => setUserType(e.target.value as UserType)}
-            className="border rounded p-2"
           >
-            <option value={UserType.ADMIN}>Admin</option>
-            <option value={UserType.USER}>User</option>
+            <option value={UserType.ADMIN}>Administrador</option>
+            <option value={UserType.USER}>Comum</option>
           </select>
         </div>
-        <button type="button" onClick={getQueryString}>
+        <button
+          type="button"
+          className="bg-blue-500 text-white h-10 px-4 rounded"
+          onClick={getQueryString}
+        >
           Buscar
         </button>
       </div>
